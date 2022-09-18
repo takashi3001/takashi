37cc745a5527187a588448a915ee0b00fcbd1491
TXKXSHI <97359762+TXKXSHI@users.noreply.github.com>
2022-09-18 13:19:12 -0300

    Add files via upload

--- jupylogiin.py
+++ jupylogiin.py
@@ -1,21 +1,37 @@
+#YOU CAN COPY AND PASTE THIS CODE
+#IMPORT THIS MODULE 
+
 import getpass
+
+#ASK IF YOU WANT CAPTURE THE USER AUTOMATICALLY
+
 condition = input("Want to capture the username automatically?: ")
 if condition == "YES" or condition == "yes" or condition == "Yes":
+    #What Happens if conditiom is correct
     user = getpass.getuser()
 else:
+    #What Happens if condition is incorrect
     user = input("User: ")
 
+#inputs password
+
 passw = getpass.getpass()
+
+#new var // print the credentials
 
 usrcredentialclient = user
 print("NEW Username: ",usrcredentialclient)
 passcredentialsclient = passw
 print("NEW Password: ",passcredentialsclient)
 
-servercredentialusr = "TAKASHI","ADM"
-servercredentialpass = "TAKASHI","ADM"
+#Credential Bank (List)
+
+servercredentialusr = None
+servercredentialpass = None
 
 if usrcredentialclient == servercredentialusr and passcredentialsclient == servercredentialpass:
+    #What Happens if credentials is correct
     print("Sucefull Login")
 else:
+    #What happens if its incorrect
     print("Wrong Pass")
